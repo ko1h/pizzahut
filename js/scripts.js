721817ba6b706cc7114ddc09724ba00bbb50409f
kohichi horii <kohichi.horii@gmail.com>
2018-11-02 09:55:35 -0700

    Add new prototype for caculaterprice

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,37 @@
-function Pizza (name, size, topping) {
-  this.name = name,
-  this.size = size,
-  this.topping = topping
+
+function PizzaOrder() {
+  this.pizzas[]
+}
+PizzaOrder.prototype.addPizzaOder = function(UserPizza) {
+  this.UserPizzas.push(UserPizza);
 }
 
+function UserPizza (name, size, topping) {
+  this.name = name,
+  this.size = size,
+  this.topping = topping,
+  this.Price = 15;
+  this.calculatePrice(totalPrice)
+}
+UserPizza.prototype.calculaterPrice = function(totalPrice) {
+  if(this.size === "orignal") {
+    this.price = 15;
+  }else if (this.size  === "large") {
+    this.price += 3; {
+      else (this.size  === "small" ) {
+        this.price -=3;
+      }
+    }
+  }
+}
 
 
 $(document).ready(function(){
   $("#pizza").submit(function(event){
     even.preventDefault();
-    var inputName = $("#nameInput").val();
-    var inputSize = $("#pizzaSize").val();
-    var inputTopping = $("#topping").val();
+    var inputName = $("input#nameInput").val();
+    var inputSize = $("input#pizzaSize").val();
+    var inputTopping = $("input#topping").val();
 
     $("#result").show();
     $("#result").text(result);
