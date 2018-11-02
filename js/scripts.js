eefbe3868ee3d1d3fcfa8a15afd536d2829b1e43
kohichi horii <kohichi.horii@gmail.com>
2018-11-02 12:30:43 -0700

    Add protype for toppiingprice and total price.

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,15 @@ function UserPizza (name, size, topping) {
   this.name = name,
   this.size = size,
   this.topping = topping
-  // this.calculatePrice(totalPrice)
-  // this.Price = 15;
+  this.sizePrice =sizePrice
+  this.toppingPrice = toppingPrice
+  this.Price = 10;
+  this.totalPrice = 0;
 }
 
-UserPizza.prototype.calculaterPrice = function(totalPrice) {
+UserPizza.prototype.sizePrice = function() {
   if(this.size === "orignal") {
-    this.price = 15;
+    this.price = 10;
     }
     else if (this.size === "Large") {
       this.price += 3;
@@ -27,22 +29,31 @@ UserPizza.prototype.calculaterPrice = function(totalPrice) {
     }
 }
 
-function populateTopping(topOptions) {
-  topOptions.forEach(function(topOption) {
-    $("#toppping").append(topOption);
-  });
+UserPizza.prototype.toppingPrice = function() {
+  forEach
 }
+
+Userpizza.prototype.total = function(){
+  this.sizePrice += this.toppingPrice
+}
+// function populateTopping(topOptions) {
+//   topOptions.forEach(function(topOption) {
+//     $("#toppping").append(topOption);
+//   });
+// }
 
 /// user interface logic
 $(document).ready(function(){
-  var topOption = ["cheese", "icecream", "wholefish", "more pizza"];
-  populateTopping(topOption);
+  // var topOption = ["cheese", "icecream", "wholefish", "more pizza"];
+  // populateTopping(topOption);
   $("#pizza").submit(function(event){
     even.preventDefault();
     var inputName = $("input#nameInput").val();
     var inputSize = $("input#pizzaSize").val();
     var inputTopping = $("input#topping").val();
+    return;
 
+    var
     $("#result").show();
     $("#result").text(result);
   });
