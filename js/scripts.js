f7d3f48937178190bf6e98045d2a57c9c18fb179
kohichi horii <kohichi.horii@gmail.com>
2018-11-02 09:11:00 -0700

    Add some business logic and some of the user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
+function Pizza (name, size, topping) {
+  this.name = name,
+  this.size = size,
+  this.topping = topping
+}
 
+
+
+$(document).ready(function(){
+  $("#pizza").submit(function(event){
+    even.preventDefault();
+    var inputName = $("#nameInput").val();
+    var inputSize = $("#pizzaSize").val();
+    var inputTopping = $("#topping").val();
+
+    $("#result").show();
+    $("#result").text(result);
+  })
+})
