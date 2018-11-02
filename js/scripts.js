0dc3eedc98282295b1feb416f67a87197f051cdf
kohichi horii <kohichi.horii@gmail.com>
2018-11-02 16:20:29 -0700

    getting a vaule back the is not the right number

--- js/scripts.js
+++ js/scripts.js
@@ -2,36 +2,33 @@ function Pizza(name, size, topping) {
   this.name = name,
   this.size = size,
   this.topping = topping
-  // this.price = 10;
-  // this.sizePrice();
-  // this.toppingPrice();
 }
 
-Pizza.prototype.sizePrice = function(size) {
+Pizza.prototype.totalPrice = function(size) {
+  var pizzaPrice =10;
   if(this.size === "orignal") {
-    this.price == 10;
+    pizzaPrice = 10;
     }
     else if (this.size === "Large") {
-      this.price += 3;
+      pizzaPrice += 3;
     }
     else if (this.size === "small") {
-      this.price -= 3;
+      pizzaPrice -=3;
     }
-}
-Pizza.prototype.toppingPrice = function(topping) {
-  if(this.topping === "icecream") {
-    this.itemPrice =+ 3;
-  }
-  else if(this.topping === "aligator") {
-    this.itemPrice =+ 7;
-  }
-  else if(this.topping === "whole fish") {
-    this.itemPrice -= 5;
-  }
-  else if(this.topping === "more pizza") {
-    this.itemPrice =+ 10;
-  }
-}
+    else if(this.topping === "icecream") {
+    pizzaPrice +=3;
+    }
+    else if(this.topping === "aligator") {
+    pizzaPrice +=5;
+    }
+    else if(this.topping === "whole fish") {
+    pizzaPrice -=2;
+    }
+    else if(this.topping === "more pizza") {
+    pizzaPrice += 10;
+    }
+    return pizzaPrice;
+};
 /// user interface logic
 $(document).ready(function(){
   $("#pizzaHut").submit(function(event){
@@ -45,7 +42,7 @@ $(document).ready(function(){
 
 
     var newPizza = new Pizza(inputName, inputSize, inputTopping);
-    $("#result").text(newPizza);
+    $("#result").text(newPizza.totalPrice());
     console.log(newPizza)
   });
 });
